3d441c31a362ebb1148cc53caa8e30972e49cc47
Marian <marian@priv.profiweb.biz>
2019-01-08 01:42:28 +0100

    In policy/feerate.cpp changed CURRENCY_UNIT from "BTC" to "VLS"

--- src/policy/feerate.cpp
+++ src/policy/feerate.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2018-2019 The Veles Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -7,7 +8,7 @@
 
 #include <tinyformat.h>
 
-const std::string CURRENCY_UNIT = "BTC";
+const std::string CURRENCY_UNIT = "VLS";
 
 CFeeRate::CFeeRate(const CAmount& nFeePaid, size_t nBytes_)
 {
