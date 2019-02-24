9029d1bced0126a481891d429a035d079b1b23b8
AltcoinBaggins <marian@priv.profiweb.biz>
2019-02-24 03:18:00 +0100

    Qt: Add alternate splash, improve layout and rendering
    Improve layout and version info on splash screen, add alternative
    splash for pre-release builds.

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -31,10 +31,11 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
     // set reference point, paddings
     int paddingRight            = 50;
     int paddingTop              = 50;
-    int titleVersionVSpace      = 17;
-    int titleCopyrightVSpace    = 40;
+    int titleVersionVSpace      = 10;
+    int titleCopyrightVSpace    = 150;
     // VE:ES BEGIN
-    int titleCopyrightWidth     = 330;
+    int titleCopyrightWidth     = 220;
+    int paddingRightCopyright   = 30;
     // VELES END
 
     float fontFactor            = 1.0;
@@ -44,24 +45,30 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
 #endif
 
     // define text to place
-    QString titleText       = tr(PACKAGE_NAME);
-    QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion()));
+    //QString titleText       = tr(PACKAGE_NAME);   // Veles edit
+    //QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion())); // Veles edit
     QString copyrightText   = QString::fromUtf8(CopyrightHolders(strprintf("\xc2\xA9 %u-%u ", 2009, COPYRIGHT_YEAR)).c_str());
     QString titleAddText    = networkStyle->getTitleAddText();
 
     QString font            = QApplication::font().toString();
 
     // VELES BEGIN
+    QPixmap splashPixmap;
+    QRect splashRect(QPoint(0,0), QSize(480,320));
+    QString titleText       = tr(PACKAGE_NAME) + " GUI";
+    QString versionText     = QString::fromStdString(FormatFullVersion()).split("-").value(0) + " \"" + CLIENT_VERSION_CODENAME + "\"";
     QString splashScreenPath = ":/images/splash";
+
+    if (!CLIENT_VERSION_IS_RELEASE)
+        splashScreenPath = ":/images/splash_prerelease";
     if(gArgs.GetBoolArg("-regtest", false))
         splashScreenPath = ":/images/splash_testnet";
     if(gArgs.GetBoolArg("-testnet", false))
         splashScreenPath = ":/images/splash_testnet";
-    QPixmap splashPixmap;
-    QRect splashRect(QPoint(0,0), QSize(480,320));
-    // Break the line at semantically correct possition
-    //copyrightText.replace("The ", " -\n  The ");
-    //copyrightText.replace("Satoshi ", " -\n  Satoshi ");
+
+    // replace for copyright sign
+    copyrightText.replace("Copyright (C)", QChar(0x00A9));
+    copyrightText.replace("Copyright (c)", QChar(0x00A9));
     // VELES END
 
     // create a bitmap according to device pixelratio
@@ -74,7 +81,7 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
 #endif
 
     QPainter pixPaint(&pixmap);
-    pixPaint.setPen(QColor(100,100,100));
+    pixPaint.setPen(QColor(30,30,30));  
 
     // draw a slightly radial gradient
     QRadialGradient gradient(QPoint(0,0), splashSize.width()/devicePixelRatio);
@@ -99,18 +106,40 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
     // VELES END
 
     // check font size and drawing with
-    pixPaint.setFont(QFont(font, 33*fontFactor));
+    pixPaint.setFont(QFont(font, 35*fontFactor));   // Veles edit
     QFontMetrics fm = pixPaint.fontMetrics();
     int titleTextWidth = fm.width(titleText);
     if (titleTextWidth > 176) {
         fontFactor = fontFactor * 176 / titleTextWidth;
     }
 
-    pixPaint.setFont(QFont(font, 33*fontFactor));
+    pixPaint.setFont(QFont(font, 35*fontFactor));   // Veles edit
     fm = pixPaint.fontMetrics();
     titleTextWidth  = fm.width(titleText);
     pixPaint.drawText(pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight,paddingTop,titleText);
 
+    // VELES BEGIN
+    // version info with codename
+    {
+        pixPaint.setFont(QFont(font, 20 * fontFactor));
+        QFontMetrics vfm = pixPaint.fontMetrics();
+        int versionTextWidth = vfm.width(versionText);
+        const int x = pixmap.width() / devicePixelRatio - versionTextWidth - paddingRight;
+        const int y = paddingTop+titleVersionVSpace;
+        QRect versionRect(x, y, pixmap.width() - x - paddingRight, paddingTop + titleCopyrightVSpace);
+        pixPaint.drawText(versionRect, Qt::AlignLeft | Qt::AlignTop, versionText);
+    }
+
+    // draw copyright stuff
+    {
+        pixPaint.setPen(QColor(100,100,100)); 
+        pixPaint.setFont(QFont(font, 15 * fontFactor));
+        const int x = pixmap.width() / devicePixelRatio - titleCopyrightWidth - paddingRightCopyright;
+        const int y = paddingTop + titleCopyrightVSpace;
+        QRect copyrightRect(x, y, pixmap.width() - x - paddingRightCopyright, pixmap.height() - y);
+        pixPaint.drawText(copyrightRect, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap, copyrightText);
+    }
+    /*
     pixPaint.setFont(QFont(font, 15*fontFactor));
 
     // if the version string is too long, reduce size
@@ -120,12 +149,8 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
         pixPaint.setFont(QFont(font, 10*fontFactor));
         titleVersionVSpace -= 5;
     }
-    pixPaint.drawText(pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight+2,paddingTop+titleVersionVSpace,versionText + " \n\"" + CLIENT_VERSION_CODENAME + "\"");
+    pixPaint.drawText(pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight,paddingTop+titleVersionVSpace,versionText);
 
-    // draw copyright stuff
-    // VELES BEGIN
-    titleTextWidth = titleCopyrightWidth;
-    // VELES END
     {
         pixPaint.setFont(QFont(font, 10*fontFactor));
         const int x = pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight;
@@ -133,6 +158,8 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
         QRect copyrightRect(x, y, pixmap.width() - x - paddingRight, pixmap.height() - y);
         pixPaint.drawText(copyrightRect, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap, copyrightText);
     }
+    */
+    // VELES END
 
     // draw additional text if special network
     if(!titleAddText.isEmpty()) {
